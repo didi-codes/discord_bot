15d8e4fa00af47fd53aa36371fa5195572c5bb55
Damaris Garcia <63378127+didi-codes@users.noreply.github.com>
2023-07-23 20:57:06 -0400

    Updated app.js for game play (#3)

--- app.js
+++ app.js
@@ -1,39 +1,46 @@
-import 'dotenv/config'
-import express from 'express'
+import 'dotenv/config';
+import express from 'express';
 import {
-    InteractionType,
-    InteractionResponseType,
-    InteractionResponseFlags,
-    MessageComponentTypes,
-    ButtonStyleTypes
+  InteractionType,
+  InteractionResponseType,
+  InteractionResponseFlags,
+  MessageComponentTypes,
+  ButtonStyleTypes,
 } from 'discord-interactions';
-import { VerifyDiscordRequest, getRandomEmoji } from './utils.js';
+import { VerifyDiscordRequest, getRandomEmoji, DiscordRequest } from './utils.js';
+import { getShuffledOptions, getResult } from './game.js';
+
 
 const app = express();
+
 const PORT = process.env.PORT || 3000;
-app.use(express.json({verify: VerifyDiscordRequest(process.env.PUBLIC_KEY)}));
 
-app.post('/interactions', async function(req, res) {
-    const { type, id, data } = req.body;
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
 
-    if(type === InteractionType.PING) {
-        return res.send({type: InteractionResponseType.PONG});
+const activeGames = {};
+
+app.post('/interactions', async function (req, res) {
+  const { type, id, data } = req.body;
+
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    // "test" command
+    if (name === 'test') {
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: 'hello world ' + getRandomEmoji(),
+        },
+      });
     }
-
-    if(type === InteractionType.APPLICATION_COMMAND) { 
-        const { name } = data;
-
-        if(name === 'test') {
-            return res.send({
-                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: 'hello world' + getRandomEmoji(),
-                },
-            });
-        }
-    }
+  }
 });
 
 app.listen(PORT, () => {
-    console.log('Listening on port', PORT)
+  console.log('Listening on port', PORT);
 });
